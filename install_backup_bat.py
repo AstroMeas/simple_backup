fea2e47188ab53b3a65fb02a4d104bf0d1cb5e82
Rafael Deichsel <49592321+AstroMeas@users.noreply.github.com>
2021-03-09 14:27:57 +0100

    cmd changed from python to start

--- install_backup_bat.py
+++ install_backup_bat.py
@@ -7,8 +7,8 @@ auto_backup = 'main_autostart.py'
 manual_backup = 'main_manualstart.py'
 
 with open('backup_autostart.bat', 'w') as auto:
-    auto.write(rf"python {working_directory}\{auto_backup}")
+    auto.write(rf"start {working_directory}\{auto_backup}")
 
 with open('backup_manualstart.bat', 'w') as auto:
-    auto.write(rf"python {working_directory}\{manual_backup}")
+    auto.write(rf"start {working_directory}\{manual_backup}")
 
